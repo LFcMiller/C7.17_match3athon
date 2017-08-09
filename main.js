a4ef16770410dfae03f666efb6824849b75f3ab1
Cody Miller <miskatonicraft@gmail.com>
2017-08-09 14:11:14 -0700

    Renamed classes on tiles - Cody

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 $(document).ready(init);
 
-var tileTypes = ["icons/chrome.png","icons/Css.png","icons/html5.png","icons/IE.png","icons/js.png","icons/php.png","icons/react.png"];
+var tileTypes = ["icons/Css.png","icons/html5.png","icons/js.png","icons/php.png","icons/react.png"];
+var specialTileTypes = ["icons/IE.png","icons/chrome.png"];
 var allTiles = [];
 var gm = null;
 function createGameBoard () {
+    $("#gameWindow").html("");
     for(var i = 0; i < 9; i++) {
         for(var j = 0; j < 9; j++) {
             var tile = new Tile(i,j);
             tile.type = tileTypes[Math.floor(Math.random()*tileTypes.length)];
-            var image = $("<img>").attr("src", tile.type).addClass("icon");
-            var containerDiv = $("<div>").addClass("tile");
+            var image = $("<img>").attr("src", tile.type).addClass("tile");
+            var containerDiv = $("<div>").addClass("position").attr({"xValue": i, "yValue": j});
             containerDiv.append(image);
             $("#gameWindow").append(containerDiv);
         }
@@ -19,7 +21,7 @@ function createGameBoard () {
 function init(){
     //TODO make all tiles, put all buttons into allTiles
     setButtonEvent(allTiles);
-    createGameBoard();
+    $(".startButton").on("click", createGameBoard);
     gm = new GameManager();
     gm.init();
     gm.startGame();
