9da7e24fa0a0c668dc9761cb7be804ae40b77958
JEN-HAO TAN <tan0929@outlook.com>
2017-08-09 21:51:50 -0700

    tiles can drop - James Tan

--- Tile.js
+++ Tile.js
@@ -1,10 +1,6 @@
 function Tile(pos) {
     this.position = pos;
-    this.shouldMove = function(){
-        if(currentPosition().x !== this.targetPosition.x || currentPosition().y !== this.targetPosition.y){
-            return true;
-        }
-    };
+    
     this.onClick;//callback
     this.type = null; //type of tile
     this.matchesWith = [];//what it can match with
@@ -12,4 +8,13 @@ function Tile(pos) {
     this.container = null;//container dom
     this.match = function(){}; //behavior on match
     this.clickFocus = false; //is element currently clicked. Will effect on-screen animation
+
+    this.drop = function(){
+        var tiles = gm.data.allTiles;
+        if(this.position.y + 1 < gm.data.boardHeight ){
+            while(tiles[this.position.y+1][this.position.x] === null){
+                gm.tileHandler.moveTile(this, new Position(this.position.x, this.position.y+1));
+            }
+        }
+    };
 }
